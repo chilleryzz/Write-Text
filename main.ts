f48821e0227c012038d4a69dc64d04278b55b9f3
chilleryzz <71833391+chilleryzz@users.noreply.github.com>
2022-01-14 10:15:08 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let APIs = false;
+let Annotations = false;
+let Blocks = APIs && Annotations;
+
+namespace basic {
+    //% block
+    export function showNumber(v: number, interval: number = 150): void { }
+}
